b06ba21807ca2676f6ae997383652c2d2f6989d1
David Velasquez <dvelas25@eafit.edu.co>
2020-02-12 13:29:48 +0100

    Complete version

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-
 app = Flask(__name__)
 CORS(app)
 
-USERS = [{'user': 'David', 'title': 'Administrator'},
-         {'user': 'Esteban', 'title': 'Brother'},
-         {'user': 'Julian', 'title': 'Developer'}
-        ]
+BOOKS = [
+    {
+        'title': ['Data mining para dummies',
+                  'Salvando mi tierra',
+                  'Comunismo despatriado'],
+        'correct_title': 'El estrés que me carcome',
+        'author': 'Camilo Torres',
+        'paperback': True
+    },
+    {
+        'title': ['Agriculture 4.0: Machine Learning',
+                  'Anomaly detection for Industry 4.0',
+                  'Visual Analytics in Wastewater Management'],
+        'correct_title': 'I love DISH//',
+        'author': 'David Velásquez',
+        'paperback': False    
+    },
+    {
+        'title': ['Quantum AI',
+                  'Rapidminer for industrials',
+                  'Creating value from statistics'],
+        'correct_title': 'Joder... eso es!',
+        'author': 'Aitor Moreno',
+        'paperback': True
+    }
+]
 
-@app.route('/api/v1/consult')
-def consult():
-    return jsonify(USERS)
+@app.route('/books', methods=['GET'])
+def books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
